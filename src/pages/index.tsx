605030f2299fdf27b10334ab0921b9fc81501614
Wolgrand Neto <wolgrand@live.com>
2021-03-25 15:54:00 -0300

    Add pages and components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,29 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
+import style from './home.module.scss'
 
 
 export default function Home() {
   return (
     <>
     <Head>
-      <title>ig.news home</title>
+      <title>Home | ig.news</title>
     </Head>
-    <h1>Hello World</h1>
+    
+    <main className={style.contentContainer}>
+      <section className={style.hero}>
+        <span>👏 Hey, welcome</span>
+        <h1>News about <br/> the <span>React </span>world.</h1>
+        <p>
+          Get acess to all the publications <br/>
+          <span>for $9.90 month</span>
+        </p>
+        <SubscribeButton />
+      </section>
+
+
+      <img src="/images/avatar.svg" alt="Girl Coding"/>
+    </main>
     </>
   )
 }
